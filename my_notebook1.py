f9d3096b30c7d4bfc582c20e5194be88359da675
arundhuti726 <arundhuti726>
2025-01-03 23:24:03 +0000

    commit test file

---
+++ my_notebook1.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+!pip install pprintpp
+
+# COMMAND ----------
+
+from pprintpp import pprint
+
+print("Hi, This is firstpost and you're watching vatange with me Palki Sharma")
+pprint("Hi, This is firstpost and you're watching vatange with me Palki Sharma")
