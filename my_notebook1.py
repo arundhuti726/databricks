de508260c14f05934a40e3009120abc4fd8e7801
arundhuti726 <arundhuti726>
2025-01-03 23:45:18 +0000

    cleaning

--- my_notebook1.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-!pip install pprintpp
-
-# COMMAND ----------
-
-from pprintpp import pprint
-
-print("Hi, This is firstpost and you're watching vatange with me Palki Sharma")
-pprint("Hi, This is firstpost and you're watching vatange with me Palki Sharma")
